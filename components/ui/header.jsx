86a32a25d6fe9f998bcadb288dd088c25d815d5c
sameernshamsudheen <sameernshamsudheen@gmail.com>
2024-11-15 17:19:03 +0530

    faeture-appheader implemented

--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -1,15 +1,41 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./button";
+import { PenBox } from "lucide-react";
+import UserMenu from "./user-menu";
+
 const Header = () => {
   return (
-    <>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </>
+    <header className="container mx-auto">
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={"/logo2.png"}
+            width={200}
+            height={56}
+            className="h-10 w-auto object-contain"
+          />
+        </Link>
+        <div className="flex items-center gap-4">
+          <Link href="/project/create">
+            <Button variant="destructive" className="flex items-center gap-2">
+              <PenBox size={18} />
+              <span>Create Project</span>
+            </Button>
+          </Link>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/onboarding">
+              <Button>Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserMenu />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
   );
 };
 
