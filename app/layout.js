9877c9c363e3dd2c423d17157dbce95032521eea
sameernshamsudheen <sameernshamsudheen@gmail.com>
2024-11-15 15:05:57 +0530

    feature-userauthenticationclerk

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
 import Header from "@/components/ui/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { shadesOfPurple } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,19 +15,25 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <Header />
+    <ClerkProvider
+      appearance={{
+        baseTheme: shadesOfPurple,
+      }}
+    >
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            <Header />
 
-          <main className="min-h-screen">{children}</main>
-          <footer className="bg-gray-900 py-12">
-            <div className="container mx-auto px-4 text-center text-gray-200">
-              <p>developed by sameer</p>
-            </div>
-          </footer>
-        </ThemeProvider>
-      </body>
-    </html>
+            <main className="min-h-screen">{children}</main>
+            <footer className="bg-gray-900 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>developed by sameer</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
