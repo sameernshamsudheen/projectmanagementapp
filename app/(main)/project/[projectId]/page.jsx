182802dbe65feeb3944a05bc6bdfd259c8898624
sameernshamsudheen <sameernshamsudheen@gmail.com>
2024-11-18 13:41:46 +0530

    feature-sprint creation

--- app/(main)/project/[projectId]/page.jsx
+++ app/(main)/project/[projectId]/page.jsx
@@ -1,4 +1,5 @@
 import { getProjectById, getProjects } from "@/actions/project";
+import SprinCreationForm from "@/components/sprint-creation-form";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -6,10 +7,28 @@ const SingleProject = async ({ params }) => {
   const { projectId } = params;
   const project = await getProjectById(projectId);
 
+  console.log(project, "====project=====");
+
   if (!project) {
     notFound();
   }
-  return <div>singleProject</div>;
+  return (
+    <div>
+      {/* // <div>sprintcreation</div>
+      // */}
+      <SprinCreationForm
+        projectTitle={project.name}
+        projectId={projectId}
+        projectKey={project.key}
+        sprintKey={project.sprints?.length + 1}
+      />
+      {project.sprints?.length === 0 ? (
+        <div> </div>
+      ) : (
+        <div>Create a sprint from button above</div>
+      )}
+    </div>
+  );
 };
 
 export default SingleProject;
